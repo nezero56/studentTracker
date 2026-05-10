d697d292b2a42ddc22b5038c339d503306fadcec
Munezero Sonia <munezerosonia85@gmail.com>
2026-05-10 21:14:08 +0200

    Implement form validation with JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+const taskForm = document.getElementById("taskForm");
+const taskInput = document.getElementById("taskInput");
+const dateInput = document.getElementById("dateInput");
+const taskContainer = document.getElementById("taskContainer");
+const errorMessage = document.getElementById("errorMessage");
+
