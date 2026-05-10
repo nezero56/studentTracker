72626db22e0177fb4079503984e5c3b10c9d992b
Munezero Sonia <munezerosonia85@gmail.com>
2026-05-10 21:16:07 +0200

    Implement form validation with JavaScript

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const dateInput = document.getElementById("dateInput");
 const taskContainer = document.getElementById("taskContainer");
 const errorMessage = document.getElementById("errorMessage");
 
+
+// Load tasks when page loads
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+// Form submit
+taskForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const taskName = taskInput.value.trim();
+  const dueDate = dateInput.value;
+
+  // Validation
+  if (taskName === "" || dueDate === "") {
+    errorMessage.classList.remove("hidden");
+    return;
+  }
+
+  errorMessage.classList.add("hidden");
+
+  const task = {
+    id: Date.now(),
+    name: taskName,
+    date: dueDate,
+  };
+  
